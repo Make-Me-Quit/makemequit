b8b0b247cae78dcdea1c8b18d4119405c5d7d48c
athibau2 <a.thibs98@gmail.com>
2022-11-01 16:31:12 -0600

    added team about descriptions

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,66 @@
 <template>
-  <v-row justify="center" align="center">
-    <h2>Hello! Meet our team!</h2>
-  </v-row>
+  <v-col class="text-center">
+    <h2 class="text">Meet our team!</h2>
+    <v-row class="picture-row text-center" justify="center" align="center">
+      <v-col v-for="(person, i) in team" :key="i">
+        <img class="team-img" :src="require(`~/assets/images/${person.img}.jpg`)" />
+        <v-spacer />
+        <span class="text">{{person.name}}</span>
+        <v-spacer />
+        <span class="text">{{person.role}}</span>
+      </v-col>
+    </v-row>
+  </v-col>
 </template>
 
 <script>
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+
+  data () {
+    return {
+      team: [
+        {
+          name: 'Jack Morgan',
+          img: 'jack',
+          role: 'Product Manager',
+        },
+        {
+          name: 'Andrew Thibaudeau',
+          img: 'andrew',
+          role: 'Software Engineer',
+        },
+        {
+          name: 'Travis Reynertson',
+          img: 'travis',
+          role: 'Software Engineer',
+        },
+        {
+          name: 'Wyatt Thacker',
+          img: 'wyatt',
+          role: 'UX Designer',
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style scoped>
+@import '~/assets/style.css';
+
+.picture-row {
+  margin-top: 20px;
+}
+
+.team-img {
+  border-radius: 50%;
+  width: 200px;
+  /* margin: 10px 10px; */
+}
+.text {
+  font-family: 'Lexend Deca';
+  color: #eaeaea;
+}
+
+</style>
